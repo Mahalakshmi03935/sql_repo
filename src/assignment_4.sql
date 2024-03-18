a2df9151b1bdb71d7ab377c33a3e892552ffbf5d
Mahalakshmi03935 <rajamahalakshmi.b@diggibyte.com>
2024-03-18 13:13:51 +0530

    Implemented  query to find gmail accounts with latest and first signup date and difference between both the dates and also write the query to replace null value with ‘1970-01-01’

---
+++ src/assignment_4.sql
@@ -0,0 +1,22 @@
+-- Creating table--
+CREATE TABLE email_signup (id INT,email_id VARCHAR(100),signup_date DATE);
+
+-- Inserting data into the table--
+INSERT INTO email_signup (id, email_id, signup_date) VALUES(1, 'Rajesh@Gmail.com', '2022-02-01'), (2, 'Rakesh_gmail@rediffmail.com', '2023-01-22'), 
+(3, 'Hitest@Gmail.com', '2020-09-08'), (4, 'Salil@Gmmail.com', '2019-07-05'), (5, 'Himanshu@Yahoo.com', '2023-05-09'), (6, 'Hitesh@Twitter.com', '2015-01-01'), 
+(7, 'Rakesh@facebook.com', null)
+
+select * from email_signup;
+
+-- setting the signup_date to '1970-01-01' where it is currently NULL
+
+UPDATE email_signup
+SET signup_date = '1970-01-01'
+WHERE signup_date IS NULL;
+
+
+SELECT COUNT(CASE WHEN email_id LIKE '%@gmail.com' THEN 1 END) AS count_gmail_account, 
+MAX(CASE WHEN email_id LIKE '%@gmail.com' THEN signup_date END) AS latest_signup_date, 
+MIN(CASE WHEN email_id LIKE '%@gmail.com' THEN signup_date END) AS first_signup_date,
+DATEDIFF(DAY, MIN(CASE WHEN email_id LIKE '%@gmail.com' THEN signup_date END), MAX(CASE WHEN email_id LIKE '%@gmail.com' THEN signup_date END)) as diff_in_days
+FROM email_signup;
