82132ed5d50358486db8e5a52a03e4b96a70d7d7
Mahalakshmi03935 <rajamahalakshmi.b@diggibyte.com>
2024-03-18 12:06:41 +0530

    state changed from unstaged to staged

--- src/assignment_2.sql
+++ src/assignment_2.sql
@@ -1,6 +1,6 @@
 --creating the table product_details
 create table product_details ( sell_date date, product varchar(100));
-
+123
 --Inserting data into the product_details table--
 INSERT INTO product_details (sell_date, product)VALUES('2020-05-30', 'Headphones'),('2020-06-01', 'Pencil'),('2020-06-02', 'Mask'),('2020-05-30', 'Basketball'),
 ('2020-06-01', 'Book'),('2020-06-02', 'Mask'),('2020-05-30', 'T-Shirt'),('2020-03-03', 'Volleyball'),('2020-04-03', 'Television'),('2020-03-06', 'Fridge');
